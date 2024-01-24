f42b1a239e4f9401df03be039735cb6e30f3d0f6
Matheus <matheus.eai@gmail.com>
2024-01-23 21:36:43 -0300

    Feat: implementing select_user

--- src/infra/db/repositories/users_repository_test.py
+++ src/infra/db/repositories/users_repository_test.py
@@ -1,6 +1,15 @@
+import pytest
+from sqlalchemy import text
+
+from src.infra.db.settings.connection import DBConnectionHandler
+
 from .users_repository import UsersRepository
 
+db_connection_handler = DBConnectionHandler()
+connection = db_connection_handler.get_engine().connect()
 
+
+@pytest.mark.skip(reason='Sensive test')
 def test_insert_user():
     mocked_first_name = 'first'
     mocked_last_name = 'last'
@@ -10,3 +19,56 @@ def test_insert_user():
     users_repository.insert_user(mocked_first_name,
                                  mocked_last_name,
                                  mocked_age)
+
+    sql = """
+        SELECT * FROM users
+        WHERE first_name = '{}'
+        AND last_name = '{}'
+        AND age = '{}'
+    """.format(
+        mocked_first_name,
+        mocked_last_name,
+        mocked_age
+        )
+    response = connection.execute(text(sql))
+    registry = response.fetchall()[0]
+    print(registry)
+
+    assert registry.first_name == mocked_first_name
+    assert registry.last_name == mocked_last_name
+    assert registry.age == mocked_age
+
+    connection.execute(text(f"""
+        DELETE FROM users WHERE id = {registry.id}
+    """))
+    connection.commit()
+
+
+def test_select_user():
+    mocked_first_name = 'first_2'
+    mocked_last_name = 'last_2'
+    mocked_age = 67
+
+    sql = """
+        INSERT INTO users (first_name, last_name, age) VALUES ('{}', '{}', {})
+    """.format(
+        mocked_first_name,
+        mocked_last_name,
+        mocked_age
+        )
+    connection.execute(text(sql))
+    connection.commit()
+
+    users_repository = UsersRepository()
+    response = users_repository.select_user(mocked_first_name)
+
+    registry = response[0]
+
+    assert registry.first_name == mocked_first_name
+    assert registry.last_name == mocked_last_name
+    assert registry.age == mocked_age
+
+    connection.execute(text(f"""
+        DELETE FROM users WHERE first_name = '{mocked_first_name}'
+    """))
+    connection.commit()
